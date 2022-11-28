173164d5a5210e711759f85d74909330a755c3a5
nefera606 <garciademata@gmail.com>
2022-11-28 11:01:25 +0100

    remove node modules

--- node_modules/metamask-react/lib/metamask-provider.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare function MetaMaskProvider(props: any): JSX.Element;
--- node_modules/metamask-react/src/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from "./metamask-provider";
-export * from "./use-metamask";
